f71a9f58e4bbc0a36a8263aee1734a046f424241
JhonnyThomazz <jhonnythomazz@gmail.com>
2026-03-05 16:38:16 -0300

    Update 05/03/26

--- app/index.tsx
+++ app/index.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text style={styles.texto}>Bem vindo!</Text>
-      <Link style={styles.btn} href="/dashboard">D ashboard</Link>
+      <Link style={styles.btn} href="/dashboard">Dashboard</Link>
       <StatusBar style="auto" />
     </View>
   );
